17642d28e67ddf5d2351a0c8686e1dd61c61dee8
Sharon <seaustin@live.unc.edu>
2014-05-24 10:39:35 -0400

    basic plugin info

---
+++ animalShelter.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Plugin Name: Animal Shelter Pets
+* Plugin URI: http://github.com/sea14/animalShelter/
+* Description: Intended for use by Animal Shelters - a custom-post type plugin that allows saving information about either cats or dogs specifically
+* Version: 1.0
+* Author: Sharon Austin
+* Author URI: http://www.sharon-elizabeth.com/
+* License: GPL 2
+* /
+
+
